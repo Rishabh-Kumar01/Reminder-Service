7fd1345723b783d80fb1cd8ffe18f6e2b4b35e14
Rishabh kumar <rishabh1teotia@gmail.com>
2024-07-23 18:59:22 +0530

    Update Sequelize data type for content field in notification ticket model and migration as it was giving error  code: 'ER_DATA_TOO_LONG', errno: 1406.
    -> Change type from STRING to TEXT("long")

--- src/models/notificationticket.js
+++ src/models/notificationticket.js
@@ -18,7 +18,7 @@ module.exports = (sequelize, DataTypes) => {
         allowNull: false,
       },
       content: {
-        type: DataTypes.STRING,
+        type: DataTypes.TEXT("long"),
         allowNull: false,
       },
       recipientEmail: {
